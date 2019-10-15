9d1fba9a52b3b5dba714bdb6750fa82aa48a8542
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-10-14 22:31:51 -0700

    tunnel.h

---
+++ tunnel.h
@@ -0,0 +1,36 @@
+#include<linux/if_tun.h>
+#include<net/if.h>
+#include<sys/ioctl.h>   
+
+
+int tun_alloc(char *dev, int flags) /*Tunnel Allocation code taken from class-provided code */
+{
+    struct ifreq ifr;
+    int fd, err;
+    char *clonedev = (char*)"/dev/net/tun";
+
+    if( (fd = open(clonedev , O_RDWR)) < 0 ) 
+    {
+	perror("Opening /dev/net/tun");
+	return fd;
+    }
+
+    memset(&ifr, 0, sizeof(ifr));
+
+    ifr.ifr_flags = flags;
+
+    if (*dev) 
+    {
+	strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+    }
+
+    if( (err = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0 ) 
+    {
+	perror("ioctl(TUNSETIFF)");
+	close(fd);
+	return err;
+    }
+
+    strcpy(dev, ifr.ifr_name);
+    return fd;
+  }
