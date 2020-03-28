752d6f56d22c8f6d144433f9546e35405998022f
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-03-27 22:57:36 -0700

    primaryrouter.h

---
+++ stage:3-7/stage:3/inc/primaryrouter.h
@@ -0,0 +1,58 @@
+#ifndef __Primary_Header__
+#define __Primary_Header__
+
+#include<string>
+#include<fstream>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<sys/select.h>
+#include<netdb.h>
+#include<stdlib.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<netinet/ip.h>
+#include<netinet/ip_icmp.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<map>
+#include "base.h"
+#include "IPinfo.h"
+#include "OctaneMessage.h"
+
+
+#define BUF_MAX_SIZE 2048
+using namespace std;
+
+
+
+class Primary_Router:public Base
+{
+    private:
+		int socket_primary_udp = 0;
+		struct sockaddr_in primaryaddr;
+		socklen_t length_udp_socket;
+		unsigned int port_number;
+		char buffertunnel[BUF_MAX_SIZE];
+		char shutdownmessage[BUF_MAX_SIZE];
+		struct timeval tv;
+		struct iphdr* ip_pac = (struct iphdr*)buffertunnel;
+		struct icmphdr* icmp_pac = (struct icmphdr *)(ip_pac + 1);
+		map<string,int>secondaryrouterinfo;
+		map<string,pair<uint8_t,uint8_t>>octanemap;
+	
+
+	public:
+		Primary_Router(string inputfile);
+		string primaryrouterfilename = "";
+		ofstream outfile_iterator;
+        int udpsocketcreation();
+        void tunnelreader();
+		void logfilewrite(string inputfile);
+		vector<string> cleanupstring(string input);
+		IPinfo *ipinfo;
+        Octane* octanemessage;
+};
+
+#endif 
