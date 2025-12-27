3882417087d479f96824fc10df85c39e48986932
shreyat <arjenbhar@gmail.com>
2025-12-27 19:46:43 +0530

    Reformat main function for improved readability and consistency

--- stage:1-2/src/main.cpp
+++ stage:1-2/src/main.cpp
@@ -12,18 +12,17 @@ int main(int argc, char* argv[])
 	}
 	Base base;
 	base.readinputfile(argv[1]);
-    Primary_Router primaryrouter(argv[1]);
-    int primaryportnumber = primaryrouter.udpsocketcreation();
-   for(int i=0;i<base.number_of_routers;i++)
-   {
-        pid_t pid_fork = fork();
-	   if(pid_fork == 0)
-	    {
-		 Secondary secondary(i+1, argv[1], primaryportnumber);
-		 secondary.secondaryudpsetup();
-	    }
-   }
-   primaryrouter.tunnelreader();
-   exit(0);
-
+	Primary_Router primaryrouter(argv[1]);
+	int primaryportnumber = primaryrouter.udpsocketcreation();
+	for(int i=0;i<base.number_of_routers;i++)
+	{
+		pid_t pid_fork = fork();
+		if(pid_fork == 0)
+		{
+			Secondary secondary(i+1, argv[1], primaryportnumber);
+			secondary.secondaryudpsetup();
+		}
+	}
+	primaryrouter.tunnelreader();
+	exit(0);
 }
